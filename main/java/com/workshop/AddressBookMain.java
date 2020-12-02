5def0134739774196bf991477f97354d48de9e25
Sippora04 <sippora.toppo95@gmail.com>
2020-12-02 10:02:43 +0530

    Added: modified the contact details

--- main/java/com/workshop/AddressBookMain.java
+++ main/java/com/workshop/AddressBookMain.java
@@ -1,19 +1,25 @@
 package com.workshop;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book Program");
 		Scanner obj = new Scanner(System.in);
 
 		ContactPerson add = new ContactPerson();
 		int ch = 0;
-		System.out.println("Enter 1 to enter into Address Book");
-		int choose = obj.nextInt();
-		while (choose==1) {
-			System.out.println("1) Enter add details");
-			System.out.println("2) Enter to edit details");
+
+		System.out.println("Welcome To Address Book Program");
+		while (true) {
+			System.out.println("Enter 1 to add details");
+			System.out.println("Enter 2 for viewing all contact details");
+			System.out.println("Enter 3 to edit details");
+			System.out.println("Enter 4 to delete details of a person");
+			System.out.println("Enter 0 to exit");
+			System.out.println("Enter anything for proceed");
+			ch = obj.nextInt();
 			if (ch == 1) {
 				Contacts p = new Contacts();
 				System.out.println("Enter First Name");
@@ -33,24 +39,48 @@ public class AddressBookMain {
 				System.out.println("Enter Email");
 				p.setEmail(obj.next());
 				add.addPerson(p);
-			}
-
-			if (ch == 2) {
-
-				System.out.println("Enter the name of person whose contact is to be modified");
-				String name = obj.next();
-				while (true) {
-					System.out.println(
-							"1. First name\n 2.Last name\n 3.Address\n 4. City\n 5. State\n 6. Zip\n 7. Phone number\n 8.Email\n 0. Exit");
-					System.out.println("Enter the info to be modified");
-					int info_name = obj.nextInt();
-					add.Modify(name, info_name);
-					if (info_name == 0) {
-						break;
+			} else if (ch == 2) {
+				ArrayList<Contacts> result = add.viewAllContacts();
+				if (result.isEmpty()) {
+					System.out.println("List is empty");
+					continue;
+				}
+				Iterator<Contacts> iter = result.iterator();
+				while (iter.hasNext()) {
+					Contacts p = (Contacts) iter.next();
+					System.out.println("First Name-" + p.getFirst_name());
+					System.out.println("Last Name-" + p.getLast_name());
+					System.out.println("Address-" + p.getAddress());
+					System.out.println("City-" + p.getCity());
+					System.out.println("State-" + p.getState());
+					System.out.println("ZIP-" + p.getZip());
+					System.out.println("Phone Number-" + p.getPhno());
+					System.out.println("Email-" + p.getEmail());
+				}
+			} else if (ch == 3) {
+				ArrayList<Contacts> result = add.viewAllContacts();
+				if (result.isEmpty()) {
+					System.out.println("List is empty");
+					continue;
+				} else {
+					System.out.println("Enter the name of person whose contact is to be edited");
+					String name = obj.next();
+					while (true) {
+						System.out.println(
+								"1. First name\n 2.Last name\n 3.Address\n 4. City\n 5. State\n 6. Zip\n 7. Phone number\n 8.Email\n 0. Exit");
+						System.out.println("Enter the information to be edit");
+						int info_name = obj.nextInt();
+						add.Modify(name, info_name);
+						if (info_name == 0) {
+							break;
+						}
 					}
 				}
-			}
-			else {
+			} else if (ch == 4) {
+				System.out.println("Enter the name of person whose contact is to be deleted");
+				String name = obj.next();
+				add.remove(name);
+			} else {
 				break;
 			}
 		}
--- main/java/com/workshop/ContactPerson.java
+++ main/java/com/workshop/ContactPerson.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ContactPerson {
 	Scanner obj = new Scanner(System.in);
-	// Array list to store contact person details
+	
 	private ArrayList<Contacts> person = new ArrayList<Contacts>();
 
 	public void setPerson(ArrayList<Contacts> person) {
@@ -16,53 +16,69 @@ public class ContactPerson {
 		return person;
 	}
 
-	// Method to add contact person details
 	public void addPerson(Contacts pobj) {
 		person.add(pobj);
 	}
-	
-	//Method to edit details
- 	public void Modify(String name,int info_name){
-		for(int i = 0; i < person.size(); i++){
-         	Contacts p = (Contacts)person.get(i); 
-         	if(name.equals(p.getFirst_name())){
-        		System.out.println("Modify Details");
-        	 	switch(info_name) {
-        	   	case 1: 
-        		   System.out.println("Enter new First Name");
-        		   p.setFirst_name(obj.next());
-        		   break;
-        	   	case 2: 
-        	   		System.out.println("Enter new Last Name");
-       				p.setLast_name(obj.next());
-       				break;
-        	   	case 3: 
-        	   		System.out.println("Enter new Address");
-       				p.setAddress(obj.next());
-       				break;
-        	   	case 4: 
-        	   		System.out.println("Enter new City");
-       				p.setCity(obj.next());
-       				break;
-        	   	case 5:
-        	   		System.out.println("Enter new State");
-       				p.setState(obj.next());
-       				break;
-        	   	case 6:
-        	   		System.out.println("Enter new Zip Code");
-        	   		p.setZip(obj.next());
-        	   		break;
-        	   	case 7:
-        	   		System.out.println("Enter new Phone Number");
-       				p.setPhno(obj.next());
-       				break;
-        	   	case 8:
-        	   		System.out.println("Enter new Email");
-       				p.setEmail(obj.next());
-       				break;
-        	   }	
-         }
-  } 
 
-}
+	public boolean isEmpty() {
+		return person.isEmpty();
+	}
+
+	public ArrayList<Contacts> viewAllContacts() {
+		return person;
+	}
+
+	public void Modify(String name, int info_name) {
+		for (int i = 0; i < person.size(); i++) {
+			Contacts p = (Contacts) person.get(i);
+			if (name.equals(p.getFirst_name())) {
+				System.out.println("Edit Details");
+				switch (info_name) {
+				case 1:
+					System.out.println("Enter new First Name");
+					p.setFirst_name(obj.next());
+					break;
+				case 2:
+					System.out.println("Enter new Last Name");
+					p.setLast_name(obj.next());
+					break;
+				case 3:
+					System.out.println("Enter new Address");
+					p.setAddress(obj.next());
+					break;
+				case 4:
+					System.out.println("Enter new City");
+					p.setCity(obj.next());
+					break;
+				case 5:
+					System.out.println("Enter new State");
+					p.setState(obj.next());
+					break;
+				case 6:
+					System.out.println("Enter new Zip Code");
+					p.setZip(obj.next());
+					break;
+				case 7:
+					System.out.println("Enter new Phone Number");
+					p.setPhno(obj.next());
+					break;
+				case 8:
+					System.out.println("Enter new Email");
+					p.setEmail(obj.next());
+					break;
+				}
+			}
+		}
+	}
+
+	public void remove(String name) {
+		for (int i = 0; i < person.size(); i++) {
+			Contacts p = (Contacts) person.get(i);
+			if (name.equals(p.getFirst_name())) {
+				person.remove(i);
+				System.out.println("Details of " + name + " removed");
+			}
+		}
+	}
+
 }
