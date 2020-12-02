9d608877c4d1413a8f5c30a612fa208b5e16e18d
Sippora04 <sippora.toppo95@gmail.com>
2020-12-02 09:35:31 +0530

    Added: Adding Contact Details

--- main/java/com/workshop/AddressBookMain.java
+++ main/java/com/workshop/AddressBookMain.java
@@ -1,7 +1,34 @@
 package com.workshop;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book Program");
+		Scanner obj = new Scanner(System.in);
+
+		ContactPerson add = new ContactPerson();
+		int ch = 0;
+
+		System.out.println("Enter Contacts add details");
+
+		Contacts p = new Contacts();
+		System.out.println("Enter First Name");
+		p.setFirst_name(obj.next());
+		System.out.println("Enter Last Name");
+		p.setLast_name(obj.next());
+		System.out.println("Enter Address");
+		p.setAddress(obj.next());
+		System.out.println("Enter City");
+		p.setCity(obj.next());
+		System.out.println("Enter State");
+		p.setState(obj.next());
+		System.out.println("Enter Zip Code");
+		p.setZip(obj.next());
+		System.out.println("Enter Phone Number");
+		p.setPhno(obj.next());
+		System.out.println("Enter Email");
+		p.setEmail(obj.next());
+		add.addPerson(p);
 	}
 }
--- main/java/com/workshop/ContactPerson.java
+++ main/java/com/workshop/ContactPerson.java
@@ -1,5 +1,33 @@
 package com.workshop;
 
-public class ContactPerson {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class ContactPerson {
+	Scanner obj = new Scanner(System.in);
+	// Array list to store contact person details
+	private ArrayList<Contacts> person = new ArrayList<Contacts>();
+
+	public void setPerson(ArrayList<Contacts> person) {
+		this.person = person;
+	}
+
+	public ArrayList<Contacts> getPerson() {
+		return person;
+	}
+
+	// Method to add contact person details
+	public void addPerson(Contacts pobj) {
+		person.add(pobj);
+	}
+
+	// Method to check if list is empty
+	public boolean isEmpty() {
+		return person.isEmpty();
+	}
+
+	// Method to check all contacts available
+	public ArrayList<Contacts> viewAllContacts() {
+		return person;
+	}
 }
