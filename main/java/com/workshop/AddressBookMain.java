7ffe5b50ec279283096ce3ff40ffe85bcc11559a
Sippora04 <sippora.toppo95@gmail.com>
2020-12-02 09:53:48 +0530

    Added: edit the contact address book

--- main/java/com/workshop/AddressBookMain.java
+++ main/java/com/workshop/AddressBookMain.java
@@ -9,26 +9,50 @@ public class AddressBookMain {
 
 		ContactPerson add = new ContactPerson();
 		int ch = 0;
+		System.out.println("Enter 1 to enter into Address Book");
+		int choose = obj.nextInt();
+		while (choose==1) {
+			System.out.println("1) Enter add details");
+			System.out.println("2) Enter to edit details");
+			if (ch == 1) {
+				Contacts p = new Contacts();
+				System.out.println("Enter First Name");
+				p.setFirst_name(obj.next());
+				System.out.println("Enter Last Name");
+				p.setLast_name(obj.next());
+				System.out.println("Enter Address");
+				p.setAddress(obj.next());
+				System.out.println("Enter City");
+				p.setCity(obj.next());
+				System.out.println("Enter State");
+				p.setState(obj.next());
+				System.out.println("Enter Zip Code");
+				p.setZip(obj.next());
+				System.out.println("Enter Phone Number");
+				p.setPhno(obj.next());
+				System.out.println("Enter Email");
+				p.setEmail(obj.next());
+				add.addPerson(p);
+			}
 
-		System.out.println("Enter Contacts add details");
+			if (ch == 2) {
 
-		Contacts p = new Contacts();
-		System.out.println("Enter First Name");
-		p.setFirst_name(obj.next());
-		System.out.println("Enter Last Name");
-		p.setLast_name(obj.next());
-		System.out.println("Enter Address");
-		p.setAddress(obj.next());
-		System.out.println("Enter City");
-		p.setCity(obj.next());
-		System.out.println("Enter State");
-		p.setState(obj.next());
-		System.out.println("Enter Zip Code");
-		p.setZip(obj.next());
-		System.out.println("Enter Phone Number");
-		p.setPhno(obj.next());
-		System.out.println("Enter Email");
-		p.setEmail(obj.next());
-		add.addPerson(p);
+				System.out.println("Enter the name of person whose contact is to be modified");
+				String name = obj.next();
+				while (true) {
+					System.out.println(
+							"1. First name\n 2.Last name\n 3.Address\n 4. City\n 5. State\n 6. Zip\n 7. Phone number\n 8.Email\n 0. Exit");
+					System.out.println("Enter the info to be modified");
+					int info_name = obj.nextInt();
+					add.Modify(name, info_name);
+					if (info_name == 0) {
+						break;
+					}
+				}
+			}
+			else {
+				break;
+			}
+		}
 	}
 }
--- main/java/com/workshop/ContactPerson.java
+++ main/java/com/workshop/ContactPerson.java
@@ -20,14 +20,49 @@ public class ContactPerson {
 	public void addPerson(Contacts pobj) {
 		person.add(pobj);
 	}
+	
+	//Method to edit details
+ 	public void Modify(String name,int info_name){
+		for(int i = 0; i < person.size(); i++){
+         	Contacts p = (Contacts)person.get(i); 
+         	if(name.equals(p.getFirst_name())){
+        		System.out.println("Modify Details");
+        	 	switch(info_name) {
+        	   	case 1: 
+        		   System.out.println("Enter new First Name");
+        		   p.setFirst_name(obj.next());
+        		   break;
+        	   	case 2: 
+        	   		System.out.println("Enter new Last Name");
+       				p.setLast_name(obj.next());
+       				break;
+        	   	case 3: 
+        	   		System.out.println("Enter new Address");
+       				p.setAddress(obj.next());
+       				break;
+        	   	case 4: 
+        	   		System.out.println("Enter new City");
+       				p.setCity(obj.next());
+       				break;
+        	   	case 5:
+        	   		System.out.println("Enter new State");
+       				p.setState(obj.next());
+       				break;
+        	   	case 6:
+        	   		System.out.println("Enter new Zip Code");
+        	   		p.setZip(obj.next());
+        	   		break;
+        	   	case 7:
+        	   		System.out.println("Enter new Phone Number");
+       				p.setPhno(obj.next());
+       				break;
+        	   	case 8:
+        	   		System.out.println("Enter new Email");
+       				p.setEmail(obj.next());
+       				break;
+        	   }	
+         }
+  } 
 
-	// Method to check if list is empty
-	public boolean isEmpty() {
-		return person.isEmpty();
-	}
-
-	// Method to check all contacts available
-	public ArrayList<Contacts> viewAllContacts() {
-		return person;
-	}
+}
 }
